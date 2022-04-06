ae7fab65245a40ff239beb82836fe2a12e388478
Georgios <georgioschochlakis@gmail.com>
2022-04-06 16:11:28 -0700

    Add predictor docstrings

--- project/predictors.py
+++ project/predictors.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 
@@ -5,18 +7,47 @@ from project.image_encoders import Embeddings
 
 
 class Predictor(nn.Module):
-    def __init__(self, encoder: Embeddings, dropout_prob=0.5, n_classes=3):
+    """Basic "binary" predictor: linear layer on top of
+    pretrained image encoder outputting values in [0, 1].
+
+    Attributes:
+        image_encoder: Image encoder.
+        ffn: dropout -> linear -> sigmoids.
+    """
+
+    def __init__(
+        self,
+        encoder: Embeddings,
+        dropout_prob: Optional[float] = 0.5,
+        n_tasks: Optional[int] = 3,
+    ):
+        """Init.
+
+        Args:
+            encoder: image encoder.
+            dropout_prob: dropout probability.
+            n_tasks: number of binary tasks.
+        """
+
         super().__init__()
 
         self.image_encoder = encoder
 
         self.ffn = nn.Sequential(
             nn.Dropout(dropout_prob),
-            nn.Linear(encoder.out_dim, n_classes),
+            nn.Linear(encoder.out_dim, n_tasks),
             nn.Sigmoid(),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward propagation.
+
+        Args:
+            x: input images.
+
+        Returns:
+            [0, 1] predictions for each task.
+        """
         x = self.image_encoder(x)
         predictions = self.ffn(x)
         return predictions
